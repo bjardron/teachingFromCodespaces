6846cfefc659f5f9a58049843558a9ad693a1cbf
bjardron <141687397+bjardron@users.noreply.github.com>
2024-12-02 08:24:54 +1100

    Create web_project_issues.py

---
+++ examples/web_project_issues.py
@@ -0,0 +1,12 @@
+"""
+Common web project issues:
+1. Missing files
+index.html references style.css but file doesn't exist
+
+2. Wrong file paths
+<img src="images/logo.png">  # But image is in assets/logo.png
+
+3. Git ignoring wrong files
+# .gitignore
+*.py  # Accidentally ignoring Python files
+"""
